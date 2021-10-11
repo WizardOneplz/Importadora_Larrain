0e905f89b880a89f6f8f249dba31698d04ba1a2c
WizardOneplz <wizardoneasd@gmail.com>
2021-10-11 19:49:13 -0300

    Mantenedor de productos v1

--- bodeguero/views.py
+++ bodeguero/views.py
@@ -1,6 +1,23 @@
 from django.shortcuts import render
+from django.db import connection
 
 # Create your views here.
 
 def mantenedor_productos(request):
-    return render(request, 'mantenedor_productos.html')
+    data = {
+        'marcas':listado_marcas()
+    }
+    return render(request, 'mantenedor_productos.html',data)
+
+def listado_marcas():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_MARCAS", [out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+
+    return lista
