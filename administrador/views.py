af4e4297e7dc12bcc0a220438426c386393be2d8
Deskraoo <electronico2_500@hotmail.com>
2021-10-12 22:29:59 -0300

    Cambios a admin y navbar

--- administrador/views.py
+++ administrador/views.py
@@ -8,6 +8,7 @@ def mantenedor_admin(request):
     data = {
         'empleados':listar_empleados(),
         'cargos':listar_cargos(),
+        'cliente':listado_clientes(),
     }
     if request.method== 'POST':
         rut = request.POST.get('rut')
@@ -32,12 +33,8 @@ def agregar_empleado(rut,nombre,ap_paterno,ap_materno,genero,telefono,email,carg
     cursor.callproc('ADM_AGREGAR_EMPLEADO',[rut, nombre, ap_paterno, ap_materno, genero, telefono, email, cargo, salida])
     return salida.getvalue()
 
-   
-
 def listar_empleados():
-    
-
-    """django_cursor = connection.cursor()
+    django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
     out_cur = django_cursor.connection.cursor()
 
@@ -46,7 +43,7 @@ def listar_empleados():
     lista = []
     for fila in out_cur:
         lista.append(fila)
-    return lista"""
+    return lista
     
 
 def listar_cargos():
@@ -57,6 +54,18 @@ def listar_cargos():
     cursor.callproc("ADM_LISTAR_CARGOS", [out_cur])
 
     lista = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listado_clientes():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur =django_cursor.connection.cursor()
+
+    cursor.callproc("LISTAR_DATOS",[out_cur])
+
+    lista =[]
     for fila in out_cur:
         lista.append(fila)
     return lista
