0ff1636e32a8a78c95a34d5854be03ee706db53f
WizardOneplz <wizardoneasd@gmail.com>
2021-10-21 21:15:22 -0300

    cambio de BD

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Bodega, Categoria, Ciudad, Cliente, CuentaCliente, CuentaEmpleado, DetalleOrden, Empleado, EstadoPago, EstadoPedido, Estanteria, Marca, Oferta, OrdenCompra, Pasillo, Producto, Region, Rol, SolicitudPresencial, SolicitudProductos, TipoOrden, TipoPago, Valoracion
+from .models import Bodega, Categoria, Ciudad, Cliente, CuentaCliente, CuentaEmpleado, DetalleOrden, Empleado, EstadoPago, EstadoPedido, EstadoSolicitud, Estanteria, Marca, Oferta, OrdenCompra, Pasillo, Producto, Region, Rol, SolicitudProductos, TipoOrden, TipoPago, Valoracion
 
 # Register your models here.
 
@@ -13,6 +13,7 @@ admin.site.register(DetalleOrden)
 admin.site.register(Empleado)
 admin.site.register(EstadoPago)
 admin.site.register(EstadoPedido)
+admin.site.register(EstadoSolicitud)
 admin.site.register(Estanteria)
 admin.site.register(Marca)
 admin.site.register(Oferta)
@@ -21,7 +22,6 @@ admin.site.register(Pasillo)
 admin.site.register(Producto)
 admin.site.register(Region)
 admin.site.register(Rol)
-admin.site.register(SolicitudPresencial)
 admin.site.register(SolicitudProductos)
 admin.site.register(TipoOrden)
 admin.site.register(TipoPago)
--- core/models.py
+++ core/models.py
@@ -4,8 +4,77 @@
 #   * Make sure each model has one field with primary_key=True
 #   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
 #   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
-# Feel free to rename the models, but don't rename db_table values or field names caca.
+# Feel free to rename the models, but don't rename db_table values or field names.
 from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255, blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128, blank=True, null=True)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150, blank=True, null=True)
+    first_name = models.CharField(max_length=150, blank=True, null=True)
+    last_name = models.CharField(max_length=150, blank=True, null=True)
+    email = models.CharField(max_length=254, blank=True, null=True)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
 
 
 class Bodega(models.Model):
@@ -44,7 +113,7 @@ class Cliente(models.Model):
     apellido_paterno = models.CharField(max_length=40)
     apellido_materno = models.CharField(max_length=40)
     genero = models.CharField(max_length=1)
-    telÚfono = models.BigIntegerField()
+    telefono = models.BigIntegerField()
     email = models.CharField(max_length=40)
     direccion = models.CharField(max_length=40)
     ciudad_id_ciudad = models.ForeignKey(Ciudad, models.DO_NOTHING, db_column='ciudad_id_ciudad')
@@ -82,10 +151,56 @@ class DetalleOrden(models.Model):
     cuenta_cliente_email = models.ForeignKey(CuentaCliente, models.DO_NOTHING, db_column='cuenta_cliente_email')
     empleado_rut = models.ForeignKey('Empleado', models.DO_NOTHING, db_column='empleado_rut')
     producto_id_producto = models.ForeignKey('Producto', models.DO_NOTHING, db_column='producto_id_producto')
+    vendedor_rut = models.CharField(max_length=4000)
 
     class Meta:
         managed = False
         db_table = 'detalle_orden'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200, blank=True, null=True)
+    action_flag = models.IntegerField()
+    change_message = models.TextField(blank=True, null=True)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100, blank=True, null=True)
+    model = models.CharField(max_length=100, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    app = models.CharField(max_length=255, blank=True, null=True)
+    name = models.CharField(max_length=255, blank=True, null=True)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField(blank=True, null=True)
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
 
 
 class Empleado(models.Model):
@@ -97,7 +212,7 @@ class Empleado(models.Model):
     genero = models.CharField(max_length=1)
     telefono = models.BigIntegerField()
     email = models.CharField(max_length=30)
-    cargo = models.BigIntegerField()
+    cargo = models.CharField(max_length=30)
 
     class Meta:
         managed = False
@@ -122,6 +237,15 @@ class EstadoPedido(models.Model):
         db_table = 'estado_pedido'
 
 
+class EstadoSolicitud(models.Model):
+    id_estado = models.BigIntegerField(primary_key=True)
+    nombre_estado = models.CharField(max_length=15)
+
+    class Meta:
+        managed = False
+        db_table = 'estado_solicitud'
+
+
 class Estanteria(models.Model):
     id_estanteria = models.BigIntegerField(primary_key=True)
     capacidad = models.BigIntegerField()
@@ -144,7 +268,7 @@ class Marca(models.Model):
 
 class Oferta(models.Model):
     id_oferta = models.BigIntegerField(primary_key=True)
-    id_proveedor = models.BigIntegerField()
+    rut_proveedor = models.CharField(max_length=20)
     nombre_proveedor = models.CharField(max_length=40)
     apellido_proveedor = models.CharField(max_length=40)
     email = models.CharField(max_length=40)
@@ -191,6 +315,7 @@ class Producto(models.Model):
     stock = models.BigIntegerField()
     oferta = models.CharField(max_length=1)
     porcentaje = models.BigIntegerField(blank=True, null=True)
+    imagen = models.BinaryField(blank=True, null=True)
     marca_id_marca = models.ForeignKey(Marca, models.DO_NOTHING, db_column='marca_id_marca')
     categoria_id_categoria = models.ForeignKey(Categoria, models.DO_NOTHING, db_column='categoria_id_categoria')
 
@@ -217,17 +342,6 @@ class Rol(models.Model):
         db_table = 'rol'
 
 
-class SolicitudPresencial(models.Model):
-    id_producto = models.BigIntegerField()
-    nombre_producto = models.CharField(max_length=30)
-    cantidad = models.BigIntegerField()
-    orden_compra_id_orden = models.OneToOneField(OrdenCompra, models.DO_NOTHING, db_column='orden_compra_id_orden', primary_key=True)
-
-    class Meta:
-        managed = False
-        db_table = 'solicitud_presencial'
-
-
 class SolicitudProductos(models.Model):
     id_solicitud = models.BigIntegerField(primary_key=True)
     nombre_producto = models.CharField(max_length=30)
@@ -237,6 +351,9 @@ class SolicitudProductos(models.Model):
     stock = models.BigIntegerField()
     observacion = models.CharField(max_length=30, blank=True, null=True)
     empleado_rut = models.ForeignKey(Empleado, models.DO_NOTHING, db_column='empleado_rut')
+    supervisor_rut = models.CharField(max_length=4000)
+    bodeguero_rut = models.CharField(max_length=4000)
+    estado_solicitud_id_estado = models.ForeignKey(EstadoSolicitud, models.DO_NOTHING, db_column='estado_solicitud_id_estado')
 
     class Meta:
         managed = False
