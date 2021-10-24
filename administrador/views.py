d2e063b70222db95f9e3645d4351f9a459399638
Deskraoo <electronico2_500@hotmail.com>
2021-10-24 20:48:03 -0300

    CAMBIO GENERAL

--- administrador/views.py
+++ administrador/views.py
@@ -19,6 +19,8 @@ def mantenedor_admin(request):
         'pasillo':listado_pasillo(),
         'estanteria':listado_estanteria()
     }
+ 
+#AGREGAR EMPLEADO
     if request.method== 'POST':
         rut = request.POST.get('rut')
         nombre = request.POST.get('nombre')
@@ -34,8 +36,39 @@ def mantenedor_admin(request):
             data['empleados'] = listado_empleados()
         else:
             data['mensaje'] = 'No se ha podido registrar al Empleado'
+
+#AGREGAR BODEGA
+    if request.method== 'POST':
+        cant_pasillos = request.POST.get('cant_pasillos')
+        direccion = request.POST.get('direccion')
+
+        salida = agregar_bodega(cant_pasillos,direccion)
+        if salida==1:
+            data['mensaje'] = 'Bodega registrada correctamente'
+            data['bodega'] = listado_bodega()
+        else:
+            data['mensaje'] = 'No se ha podido registrar la bodega'
+
+#AGREGAR PASILLO
+    if request.method== 'POST':
+        cant_estanterias = request.POST.get('cant_estanterias')
+        id_bodega = request.POST.get('id_bodega')
+
+        salida = agregar_pasillo(cant_estanterias,id_bodega)
+        if salida==1:
+            data['mensaje'] = 'Pasillo registrado correctamente'
+            data['pasillo'] = listado_pasillo()
+        else:
+            data['mensaje'] = 'No se ha podido registrar el pasillo'
+
+#AGREGAR ESTANTERIA
+
     return render(request, 'agregar_empleado.html',data)
 
+
+#METODOS
+
+#EMPLEADO
 def agregar_empleado(rut,nombre,ap_paterno,ap_materno,genero,telefono,email,cargo):
     django_cursor = connection.cursor()
     cursor = django_cursor.connection.cursor()
@@ -54,6 +87,7 @@ def listado_empleados():
     for fila in out_cur:
         lista.append(fila)
     return lista
+    
 
 def listar_cargos():
     django_cursor = connection.cursor()
@@ -61,42 +95,6 @@ def listar_cargos():
     out_cur = django_cursor.connection.cursor()
 
     cursor.callproc("ADM_LISTAR_CARGOS", [out_cur])
-
-    lista = []
-    for fila in out_cur:
-        lista.append(fila)
-    return lista
-
-def listar_marcas():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-
-    cursor.callproc("SP_LISTAR_MARCAS", [out_cur])
-
-    lista = []
-    for fila in out_cur:
-        lista.append(fila)
-    return lista
-
-def listar_categorias():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-
-    cursor.callproc("SP_LISTAR_CATEGORIAS", [out_cur])
-
-    lista = []
-    for fila in out_cur:
-        lista.append(fila)
-    return lista
-
-def listar_productos():
-    django_cursor = connection.cursor()
-    cursor = django_cursor.connection.cursor()
-    out_cur = django_cursor.connection.cursor()
-
-    cursor.callproc("SP_LISTAR_PRODUCTOS", [out_cur])
 
     lista = []
     for fila in out_cur:
@@ -144,8 +142,7 @@ def editar_empleado(request):
     empleado.save()
 
     return redirect('/agregar_empleado')
-
-#CLIENTES
+#CLIENTE
 
 def listado_clientes():
     django_cursor = connection.cursor()
@@ -159,7 +156,52 @@ def listado_clientes():
         lista.append(fila)
     return lista
 
+#PRODUCTOS
+
+def listar_productos():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_PRODUCTOS", [out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listar_marcas():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_MARCAS", [out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
+def listar_categorias():
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    out_cur = django_cursor.connection.cursor()
+
+    cursor.callproc("SP_LISTAR_CATEGORIAS", [out_cur])
+
+    lista = []
+    for fila in out_cur:
+        lista.append(fila)
+    return lista
+
 #BODEGA
+
+def agregar_bodega(cant_pasillos,direccion):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('ADM_AGREGAR_BODEGA',[cant_pasillos, direccion, salida])
+    return salida.getvalue()
 
 def listado_bodega():
     django_cursor = connection.cursor()
@@ -173,7 +215,22 @@ def listado_bodega():
         lista.append(fila)
     return lista
 
+def eliminar_empleado(request, rut):
+    cuentaempleado = CuentaEmpleado.objects.get(empleado_rut=rut)
+    cuentaempleado.delete()
+    empleado = Empleado.objects.get(rut=rut)
+    empleado.delete()
+
+    return redirect('/agregar_empleado')
+
 #PASILLO
+
+def agregar_pasillo(cant_estanterias,id_bodega):
+    django_cursor = connection.cursor()
+    cursor = django_cursor.connection.cursor()
+    salida = cursor.var(cx_Oracle.NUMBER)
+    cursor.callproc('ADM_AGREGAR_PASILLO',[cant_estanterias,id_bodega, salida])
+    return salida.getvalue()
 
 def listado_pasillo():
     django_cursor = connection.cursor()
