82cdbd72ffaa1ff3ffb8c7a127177f66aa916470
Deskraoo <electronico2_500@hotmail.com>
2021-11-11 23:10:08 -0300

    RESTRICCIONES GENERALES

--- shared/static/app/js/formulario.js
+++ shared/static/app/js/formulario.js
@@ -19,58 +19,28 @@
         })
     })() 
 
-    //VALIDACION EXTENSION DE TELEFONO
-    document.getElementById('telefono').onkeypress =
-      function (e) {
-        var ev = e || window.event;
-        if(ev.charCode < 48 || ev.charCode > 57) {
-          return false; // not a digit
-        } else if(this.value * 10 + ev.charCode - 48 > this.max) {
-           return false;
-        } else {
-           return true;
+
+    function buscaEmpleado() {
+        // Declare variables
+        var input, filter, table, tr, td, i, txtValue;
+        input = document.getElementById("myInput");
+        filter = input.value.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+
+        // Loop through all table rows, and hide those who don't match the search query
+        for (i = 0; i < tr.length; i++) {
+            td = tr[i].getElementsByTagName("td")[0];
+            if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+                tr[i].style.display = "none";
+            }
+            }
         }
-      }
-
-    //VALIDACION EXTENSION DE rut
-    document.getElementById('rut').onkeypress =
-    function (e) {
-      var ev = e || window.event;
-      if(ev.charCode < 48 || ev.charCode > 57) {
-        return false; // not a digit
-      } else if(this.value * 10 + ev.charCode - 48 > this.max) {
-         return false;
-      } else {
-         return true;
-      }
-    }
-
-    //VALIDACION EXTENSION DE stock
-    document.getElementById('stock').onkeypress =
-      function (e) {
-        var ev = e || window.event;
-        if(ev.charCode < 48 || ev.charCode > 57) {
-          return false; // not a digit
-        } else if(this.value * 10 + ev.charCode - 48 > this.max) {
-           return false;
-        } else {
-           return true;
         }
-      }
-
-    //VALIDACION EXTENSION DE stock
-    document.getElementById('precio').onkeypress =
-    function (e) {
-      var ev = e || window.event;
-      if(ev.charCode < 48 || ev.charCode > 57) {
-        return false; // not a digit
-      } else if(this.value * 10 + ev.charCode - 48 > this.max) {
-         return false;
-      } else {
-         return true;
-      }
-    }
-
     
 
       
