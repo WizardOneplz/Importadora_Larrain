335878ab97fed229544cceea62c7d10cd635bd1e
Deskraoo <electronico2_500@hotmail.com>
2021-11-25 23:25:28 -0300

    CAMBIOS DE BD, MODELS Y TABLAS

--- registro/views.py
+++ registro/views.py
@@ -71,14 +71,9 @@ def cerrarsesion(request):
 
 def modificar_cliente(request, cliente_rut):
     data={
-<<<<<<< HEAD
         'cliente' : Cliente.objects.get(rut=cliente_rut),
         'cuentacliente': CuentaCliente.objects.get(cliente_rut=cliente_rut),
         'CIUDAD':listar_ciudad() 
-=======
-        'cliente' : Cliente.objects.get(rut= cliente_rut),
-        'cuentacliente': CuentaCliente.objects.get(cliente_rut= cliente_rut)
->>>>>>> 9b0781526347261675032c3659d6ca0ae4a4f4a4
     }
     return render(request, "perfil.html", data)
 
