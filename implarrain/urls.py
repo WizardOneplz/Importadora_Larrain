74a0b902aaf842e4c99d25dec2be522b74ab9d07
Deskraoo <electronico2_500@hotmail.com>
2021-12-06 21:09:46 -0300

    cambios a url

--- implarrain/urls.py
+++ implarrain/urls.py
@@ -29,11 +29,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',home),
     path('store', store),
-<<<<<<< HEAD
-    path('search', search),
-=======
     path('cart/', include('cart.urls', namespace='cart')),
->>>>>>> 7cff1de4d538d8eebede24d485152b32a1707bb4
     path('producto/<int:pk>/', producto, name='producto'),
     path('oferta', oferta),
 
